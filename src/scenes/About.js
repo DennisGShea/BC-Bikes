dc64bcca325d744ef9a8518495989b238996cfd1
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-07-13 15:00:02 -0400

    first commit

---
+++ src/scenes/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section>
+            <h1>About</h1>
+            <p>Great Bike Shop, cycle by!</p>
+        </section>
+    )
+}
+
+export default About
---
+++ src/scenes/Shop.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+function Shop() {
+    const { category } = useParams()
+    return (
+        <section>
+            <h1>Shop</h1>
+            <Link to="/shop/road">Road</Link>
+            <h1>Shop</h1>
+            <Link to="/shop/bmx">BMX</Link>
+            <h1>Shop</h1>
+            <Link to="/shop/hybrid">Hybrid</Link>
+            &nbsp;|&nbsp;
+            <Link to="/shop/electric">Electric</Link>
+            {category
+                ? <h2>{category.toUpperCase()}</h2>
+                : <p>Choose a category</p>
+            }
+        </section>
+    )
+}
+export default Shop
